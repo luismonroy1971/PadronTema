afa73f72987aeefbece1176b47999d66e11a943f
luismonroy1971 <lmonroy1971@gmail.com>
2024-07-16 15:22:13 -0500

    Mostrar de color verde la opción activa del sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, {
-  useState
-} from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Users from './components/Users';
@@ -19,7 +17,7 @@ import Projects from './components/tables/Projects';
 import Periodic from './components/evaluation/Periodic';
 import OnDemand from './components/evaluation/OnDemand';
 import LoginPage from './components/LoginPage';
-import OptionsByRoles from './components/OptionsByRoles'; // Importar el nuevo componente
+import OptionsByRoles from './components/OptionsByRoles';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -28,117 +26,38 @@ function App() {
     setIsAuthenticated(true);
   };
 
-  return ( <
-    Router >
-    <
-    div className = "flex h-screen" > {
-      isAuthenticated ? ( <
-        >
-        <
-        Sidebar / >
-        <
-        div className = "flex-grow p-4" >
-        <
-        Routes >
-        <
-        Route path = "/users"
-        element = {
-          <
-          Users / >
-        }
-        /> <
-        Route path = "/roles"
-        element = {
-          <
-          Roles / >
-        }
-        /> <
-        Route path = "/options"
-        element = {
-          <
-          Options / >
-        }
-        /> <
-        Route path = "/options-by-roles"
-        element = {
-          <
-          OptionsByRoles / >
-        }
-        /> <
-        Route path = "/basic-info"
-        element = {
-          <
-          BasicInfo / >
-        }
-        /> <
-        Route path = "/human-resources"
-        element = {
-          <
-          HumanResources / >
-        }
-        /> <
-        Route path = "/payroll"
-        element = {
-          <
-          Payroll / >
-        }
-        /> <
-        Route path = "/tables/master-data"
-        element = {
-          <
-          MasterData / >
-        }
-        /> <
-        Route path = "/tables/projects"
-        element = {
-          <
-          Projects / >
-        }
-        /> <
-        Route path = "/evaluation/periodic"
-        element = {
-          <
-          Periodic / >
-        }
-        /> <
-        Route path = "/evaluation/on-demand"
-        element = {
-          <
-          OnDemand / >
-        }
-        /> <
-        Route path = "*"
-        element = {
-          <
-          Navigate to = "/users" / >
-        }
-        /> < /
-        Routes > <
-        /div> < /
-        >
-      ) : ( <
-          Routes >
-          <
-          Route path = "/"
-          element = {
-            <
-            LoginPage onLogin = {
-              handleLogin
-            }
-            />} / >
-            <
-            Route path = "*"
-            element = {
-              <
-              Navigate to = "/" / >
-            }
-            /> < /
-            Routes >
-          )
-        } <
-        /div> < /
-        Router >
-    );
-  }
+  return (
+    <Router>
+      <div className="flex h-screen">
+        {isAuthenticated ? (
+          <>
+            <Sidebar />
+            <div className="flex-grow p-4">
+              <Routes>
+                <Route path="/users" element={<Users />} />
+                <Route path="/roles" element={<Roles />} />
+                <Route path="/options" element={<Options />} />
+                <Route path="/options-by-roles" element={<OptionsByRoles />} />
+                <Route path="/basic-info" element={<BasicInfo />} />
+                <Route path="/human-resources" element={<HumanResources />} />
+                <Route path="/payroll" element={<Payroll />} />
+                <Route path="/tables/master-data" element={<MasterData />} />
+                <Route path="/tables/projects" element={<Projects />} />
+                <Route path="/evaluation/periodic" element={<Periodic />} />
+                <Route path="/evaluation/on-demand" element={<OnDemand />} />
+                <Route path="*" element={<Navigate to="/users" />} />
+              </Routes>
+            </div>
+          </>
+        ) : (
+          <Routes>
+            <Route path="/" element={<LoginPage onLogin={handleLogin} />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        )}
+      </div>
+    </Router>
+  );
+}
 
-  export default App;
+export default App;
